6af98618739263683c5a858b034114c3669338cb
Javiera <javiera19.navarrete@gmail.com>
2022-11-30 11:02:34 -0300

    fin de la refactorizacion

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config: 
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLACHEMY_DATABASE_URI = os.environ.get('DATEBASE_URI')\
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')\
         or 'sqlite://' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False

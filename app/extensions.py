7ab701b3e91c7a43f8bc5d0ebeef8a0ce87397bf
Javiera <javiera19.navarrete@gmail.com>
2022-12-06 11:01:01 -0300

    flask

--- app/extensions.py
+++ app/extensions.py
@@ -1,5 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 db =SQLAlchemy()
 migrate = Migrate()
+
+login_manager 0 LoginManager()
+login_manager.login_view 0 'auth.login'
